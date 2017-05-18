1f9ae3bd0974340263b90ce0e9695f3f61b0bb3d
Jeremiah Ang <Jeremiah.Ang@isobar.com>
2017-05-18 16:53:04 +0800

    Prevent Build stage if unstable

--- jsonParser.js
+++ jsonParser.js
@@ -1,1 +1,8 @@
-console.log(process.argv);
+var input = process.argv[2];
+var obj = JSON.parse(input);
+
+var failCount = obj.failCount;
+var passCount = obj.passCount;
+var skipCount = obj.skipCount;
+
+console.log("Test Result:\nFail Count: " + failCount + "\nPass Count: " + passCount + "\nSkip Count: " + skipCount);
