0cb6ed36115ba797d342337a9987d0151fb92e39
Jeremiah Ang <Jeremiah.Ang@isobar.com>
2017-05-18 17:55:42 +0800

    Prevent Build stage if unstable

--- jenkins/jsonParser.js
+++ jenkins/jsonParser.js
@@ -4,8 +4,7 @@ var filePath = path.resolve(__dirname, "jenkinsInput.in");
 
 fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
     if (!err) {
-    	data = data.substr(0, data.length - 1);
-        var json = JSON.parse(data.trim());
+        var json = JSON.parse(data);
 		console.log("Test Result: \nFail Count: " + json.failCount + "\nPass Count: " + json.passCount + "\nSkip Count: " + json.skipCount);
     } else {
         console.log(err);
