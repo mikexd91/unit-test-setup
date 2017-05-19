d5a40813ec6c77779547c6ffce7974f7cb7e82cf
Jeremiah Ang <Jeremiah.Ang@isobar.com>
2017-05-19 12:48:25 +0800

    Rocketchat send eslint result

--- app/test/reports/rocketchat/jsonParser.js
+++ app/test/reports/rocketchat/jsonParser.js
@@ -1,17 +1,18 @@
 var fs = require('fs');
 var path = require('path');
 
-var filePaths = require ('../reports_filepath.js');
-
-for (var type in filePaths.types) {
-	if (type != 'coverage') {
-		var outputFilePath = path.resolve (__dirname, "../../" + filePaths.types[type].outputFile.json);
-		readFile(outputFilePath, type);
+function Rocketchat () {
+	var filePaths = require ('../reports_filepath.js');
+	for (var type in filePaths.types) {
+		if (type != 'coverage') {
+			var outputFilePath = path.resolve (__dirname, "../../" + filePaths.types[type].outputFile.json);
+			readFile(outputFilePath, type);
+		}
 	}
 }
 
 function readFile (file, type) {
-	fs.readFile(outputFilePath, {encoding: 'utf-8'}, function(err,data){
+	fs.readFile(file, {encoding: 'utf-8'}, function(err,data){
 		if (!err) {
 		    processData(type, data);
 		} else {
@@ -33,7 +34,12 @@ function processData(type, data) {
 
 function processUnitTest (json) {
 	var summary = json.summary;
-	console.log("Test Result\nSuccess: " + summary.success + "\nFailed: " + summary.failed);
+	printResult({
+		"Test Result:" : {
+			"Success:" : summary.success,
+			"Failed:" : summary.failed
+		}
+	})
 }
 
 function processEslint (json) {
@@ -43,7 +49,32 @@ function processEslint (json) {
 		warningCount += data.warningCount;
 	});
 
-	console.log("Lint Result:");
-	console.log("Error Count: " + errorCount + "\nWarning Count: " + warningCount);
-	console.log();
+	printResult({
+		"Lint Result:" : {
+			"Error Count:" : errorCount,
+			"Warning Count:" : warningCount
+		}
+	})
 }
+
+function printResult (resultObj, space = "") {
+	if (typeof resultObj != 'object') {
+		console.log (space + resultObj);
+	} else {
+		for (var key in resultObj) {
+
+			if (typeof resultObj[key] == 'object') {
+				console.log(space + key);
+				//recursive call
+				printResult (resultObj[key], space + "   ");
+			} else {
+				console.log (space + key + resultObj[key]);
+			}
+		}
+	}
+
+	if (space == "")
+		console.log();
+}
+
+Rocketchat();
