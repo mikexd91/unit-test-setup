743b6f339adedf72dccbf97a64b9104c24a24940
Jeremiah Ang <Jeremiah.Ang@isobar.com>
2017-05-19 17:27:43 +0800

    Suite tests

--- karma.conf.js
+++ karma.conf.js
@@ -55,7 +55,7 @@ module.exports = function(config) {
         outputDir: types.unitTest.outputDir.junit, // './reports/unit-test/junit',
         outputFile: types.unitTest.filename.junit, // unit-test-report.xml
         classNameFormatter: function (browser, result) {
-            return result.suite.join('.');
+            return browser+'.'+result.suite.join('.');
         }
     },
 
