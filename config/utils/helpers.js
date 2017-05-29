88c527180d3b1fe74f1779f91dfb36eca5d73736
Jeremiah Ang <Jeremiah.Ang@isobar.com>
2017-05-29 11:23:12 +0800

    automation testing

--- config/utils/helpers.js
+++ config/utils/helpers.js
@@ -83,7 +83,7 @@ module.exports.merge = function (original, newer) {
 module.exports.setGitRemoteURL = function (url) {
 	var exec = require ("child_process").exec;
 	exec ("rmdir .git /s /q &&" + 
-		  "git branch -m master " +
+		  "git init && git branch -m master " +
 		  "&& git remote set-url origin " + url, function (error, stdout, stderr) {
 		if (!error) {
 			console.log (stdout);
